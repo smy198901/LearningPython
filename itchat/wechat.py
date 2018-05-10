3a53e15ba3d4307cdacf2cfeaa685ae04ce3a3d6
smy198901 <15651101794@wo.cn>
2018-05-10 21:07:32 +0800

    更新

--- itchat/wechat.py
+++ itchat/wechat.py
@@ -4,6 +4,10 @@ import xlrd
 import pymysql
 import os
 
+DATABASE_URL = "localhost"
+DATABASE_USERNAME = "root"
+DATABASE_PASSWORD = "root"
+DATABASE_NAME = "zhwhb"
 
 #自动添加好友，并打招呼
 @itchat.msg_register(FRIENDS)
@@ -14,24 +18,41 @@ def auto_add_friend(msg):
 #根据关键字自动回复
 @itchat.msg_register([TEXT,PICTURE,NOTE,RECORDING,ATTACHMENT,VIDEO])
 def auto_reply(msg):
-	db = pymysql.connect("localhost","root","root","zhwhb")
-	cursor = db.cursor()
 	if msg['Type'] == 'Text':
 		message = msg['Text']#获取文本信息
 		reply_msg = '[自动回复]'
+
+		if message == "天气":
+			pass
+
+
 		if message[0:3] == "问题-":
-			#当符合问题关键字时，做以下操作
-			str_message = message[3:]
-			cursor.execute("SELECT *FROM cost limit 2")
-			data = cursor.fetchall()#获取所有结果
-			for row in data:
-				reply_msg = reply_msg + "\n" + str(row[0]) + ":" + str(row[1])
-		itchat.send_msg(reply_msg,toUserName=msg['FromUserName'])
+			reply_msg = answer_question(message)
 
-		db.close()
+		itchat.send_msg(reply_msg,toUserName='filehelper')
+
 	else:
-		itchat.send_msg("[自动回复]\n无法识别",toUserName=msg['FromUserName'])
-		itchat.send_msg("[自动回复]\n123",toUserName=msg['FromUserName'])
+		#itchat.send_msg("[自动回复]\n无法识别",toUserName=msg['FromUserName'])
+		#itchat.send_msg("[自动回复]\n123",toUserName=msg['FromUserName'])
+		pass
 
-itchat.auto_login(hotReload=True)
+#回答问题，关键字类型“问题-”
+def answer_question(message):
+	db = pymysql.connect(DATABASE_URL,DATABASE_USERNAME,DATABASE_PASSWORD,DATABASE_NAME)
+	cursor = db.cursor()
+	reply_msg = '[自动回复]'
+	str_message = message[3:]
+	cursor.execute("SELECT *FROM cost limit 2")
+	data = cursor.fetchall()#获取所有结果
+	for row in data:
+		reply_msg = reply_msg + "\n" + str(row[0]) + ":" + str(row[1])
+	db.close()
+	return reply_msg
+
+#获取指定城市的天气
+def get_weather(message):
+	pass
+
+
+itchat.auto_login(hotReload=True,enableCmdQR=2)
 itchat.run()
